8b1077bbe9e1c00b0de13620d80c9b6eb56cfb0a
Dylan Di Filppo <dylan.di.filippo@gmail.com>
2020-07-01 11:00:02 +0200

    created the components, the model and the service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExerciseOneComponent } from './exercise-one/exercise-one.component';
+import { ExerciseTwoComponent } from './exercise-two/exercise-two.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExerciseOneComponent,
+    ExerciseTwoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/exercise-two/exercise-two.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exercise-two',
+  templateUrl: './exercise-two.component.html',
+  styleUrls: ['./exercise-two.component.css']
+})
+export class ExerciseTwoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
